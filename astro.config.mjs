4e67337207ed013774641f00a1811a4f2bf03465
Dakota Whipple <dakotajaywhipple@gmail.com>
2025-05-26 12:23:19 -0400

    Update site configuration and add CNAME for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://dakotawhipple.github.io',
-	base: '/docs',
+	site: 'https://whipple.dev',
 	integrations: [
 		starlight({
 			title: 'My Docs',
