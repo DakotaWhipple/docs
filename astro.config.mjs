a2b12b96f36fa5c2da34d7bd40802bbdc5a039ec
Dakota Whipple <dakotajaywhipple@gmail.com>
2025-05-26 12:06:48 -0400

    Add site and base configuration to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://dakotawhipple.github.io',
+	base: '/docs',
 	integrations: [
 		starlight({
 			title: 'My Docs',
